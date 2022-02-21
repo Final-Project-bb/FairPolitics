e4ccd57111b58943dfefdcbfa0993ddd331124f5
shaiBonfil <shaibonfil500@gmail.com>
2022-02-21 10:35:45 +0200

    push before work

--- Server/routes/login.js
+++ Server/routes/login.js
@@ -9,6 +9,7 @@ router.get("/get_user/:user_id", getUser);
 router.put("/update_user/:user_id", updateUser);
 router.delete("/delete_user/:user_id", deleteUser);
 router.put("/add_user/", auth);
+
 router.get("/get_following/:user_id", getFollowing);
 router.get("/get_followers/:user_id", getFollowers);
 
