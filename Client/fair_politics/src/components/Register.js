5ed9d9b80591d90135b7d8a0ff160e9f28d57154
talschreiber95 <talfreestyle@gmail.com>
2022-02-23 11:15:36 +0200

    Update: client side's view and structure match to others side

--- Client/fair_politics/src/components/Register.js
+++ Client/fair_politics/src/components/Register.js
@@ -4,17 +4,37 @@ import { useState } from "react";
 import { useHistory } from "react-router-dom";
 import styled from 'styled-components';
 const Register = () => {
+    const [id, setId] = useState();
     const [phone, setPhone] = useState();
+    const [pass, setPass] = useState();
+    const [pass2, setPass2] = useState();
     const [tempPass, setTempPass] = useState();
     const [tempPassFromDB, setTempPassFromDB] = useState();
     const [firstName, setFirstName] = useState();
     const [lastName, setLastName] = useState();
     const [location, setLocation] = useState();
     const [rule, setRule] = useState();
+    const [date, setDate] = useState();
     const [picture, setPicture] = useState();
-
+    const [gender, setGender] = useState();
+    const current = new Date().toISOString().split("T")[0]
     // const [approve]  
-
+    const userInfo ={
+        user_id: id,
+        first_name: firstName,
+        last_name: lastName,
+        city: location,
+        birthdate: date,
+        job_title: rule,
+        description: "None yet",
+        profile_picture: picture,
+        gender: gender,
+      }
+      const userLogin ={
+        user_id: id,
+        phone_number:phone,
+        password: pass,
+      }
     const phoneSubmit = (event) => {
         event.preventDefault();
         setTempPassFromDB("1225") // here should get the temp pass from server
@@ -35,13 +55,22 @@ const Register = () => {
             alert(`Password failed`)
         }
     }
+    const approvePassword = (event) => {
+        event.preventDefault();
+        if (pass2 === pass) {
+            alert(`Password approved`)
+        }
+        else {
+            alert(`Password failed`)
+        }
+    }
     const registerSubmit = (event) => {
-        const newUser={
-            firstName:firstName,
-            lastName:lastName,
-            location:location,
-            rule:rule,
-            picture:picture
+        const newUser = {
+            firstName: firstName,
+            lastName: lastName,
+            location: location,
+            rule: rule,
+            picture: picture
         }
         addNewUser(newUser)
     }
@@ -55,85 +84,150 @@ const Register = () => {
     return (
         <div>
             <Header title="Register Page" />
-            <RegisterFormStyle style={{position: "relative"}}>
-            <form onSubmit={phoneSubmit}>
-                <label>Enter a Phone Number:</label>
-                <input
-                    type="tel"
-                    pattern="[0]{1}[5]{1}[0-9]{8}"
-                    required
-                    placeholder='valid phone number!'
-                    value={phone}
-                    onChange={(e) => setPhone(e.target.value)}
-                    className='inputPhoneNum'
-                />
-                <small>05... requird Exmple: 0544567891</small><br />
-                <input type="submit" />
-            </form>
-            <form onSubmit={approveSubmit}>
-                <label>Enter a temporary password:</label>
-                <input
-                    type="password"
-                    // pattern="[0]{1}[5]{1}[0-9]{8}"
-                    // required
-                    placeholder='password from sms!'
-                    value={tempPass}
-                    onChange={(e) => setTempPass(e.target.value)}
-                />
-                <br />
-                <input type="submit" />
-            </form>
-            <form onSubmit={registerSubmit}>
-                <label>Enter your first name:</label>
-                <input
-                    type="text"
-                    // pattern="[0]{1}[5]{1}[0-9]{8}"
-                    // required
-                    placeholder='first name!'
-                    value={firstName}
-                    onChange={(e) => setFirstName(e.target.value)}
-                /><br />
-                <label>Enter your last name:</label>
-                <input
-                    type="text"
-                    // pattern="[0]{1}[5]{1}[0-9]{8}"
-                    // required
-                    placeholder='last name!'
-                    value={lastName}
-                    onChange={(e) => setLastName(e.target.value)}
-                /><br />
-                <label>Enter your location:</label>
-                <input
-                    type="text"
-                    // pattern="[0]{1}[5]{1}[0-9]{8}"
-                    // required
-                    placeholder='location!'
-                    value={location}
-                    onChange={(e) => setLocation(e.target.value)}
-                /><br />
-                <label>Enter your rule:</label>
-                <input
-                    type="text"
-                    // pattern="[0]{1}[5]{1}[0-9]{8}"
-                    // required
-                    placeholder='Rule!'
-                    value={rule}
-                    onChange={(e) => setRule(e.target.value)}
-                /><br />
-                <label>Insert your Profile's picture:</label>
-                <input
-                    type="text"
-                    // pattern="[0]{1}[5]{1}[0-9]{8}"
-                    // required
-                    placeholder='Picture!'
-                    value={picture}
-                    onChange={(e) => setPicture(e.target.value)}
-                /><br />
-                <input type="submit" />
-            </form>
-            <button>Facebook</button><br />
-            <button>Gmail</button><br />
-            <button onClick={handleClick}>already have an account ? sign in here!</button>
+            <FirstRegisterFormStyle style={{ position: "relative" }}>
+                <form onSubmit={phoneSubmit}>
+                    <label>Enter your id:</label>
+                    <input
+                        type="tel"
+                        pattern="[0-9]{9}"
+                        required
+                        placeholder='valid id number!'
+                        value={id}
+                        onChange={(e) => setId(e.target.value)}
+                        className='inputId'
+                    /><br />
+                    <label>Enter a Phone Number:</label>
+                    <input
+                        type="tel"
+                        pattern="[0]{1}[5]{1}[0-9]{8}"
+                        required
+                        placeholder='valid phone number!'
+                        value={phone}
+                        onChange={(e) => setPhone(e.target.value)}
+                        className='inputPhoneNum'
+                    /><br />
+                    <input type="submit" />
+                </form>
+                <form onSubmit={approveSubmit}>
+                    <label>Enter a temporary password:</label><br />
+                    <input
+                        type="password"
+                        // pattern="[0]{1}[5]{1}[0-9]{8}"
+                        // required
+                        placeholder='password from sms!'
+                        value={tempPass}
+                        onChange={(e) => setTempPass(e.target.value)}
+
+                    />
+                    <input type="submit" />
+                </form><br/>
+                <form onSubmit={approvePassword}>
+                    <label>Enter your final password:</label><br />
+                    <input
+                        type="password"
+                        // pattern="[0]{1}[5]{1}[0-9]{8}"
+                        // required
+                        placeholder='your password!'
+                        value={pass}
+                        onChange={(e) => setPass(e.target.value)}
+
+                    /><br />
+                    <label>Enter your final password again:</label><br />
+                    <input
+                        type="password"
+                        // pattern="[0]{1}[5]{1}[0-9]{8}"
+                        // required
+                        placeholder='repet your password!'
+                        value={pass2}
+                        onChange={(e) => setPass2(e.target.value)}
+                    />
+                    <input type="submit" />
+                </form>
+            </FirstRegisterFormStyle>
+            <RegisterFormStyle>
+
+                <form onSubmit={registerSubmit}>
+                    <label>Enter your first name:</label>
+                    <input
+                        type="text"
+                        // pattern="[0]{1}[5]{1}[0-9]{8}"
+                        // required
+                        placeholder='first name!'
+                        value={firstName}
+                        onChange={(e) => setFirstName(e.target.value)}
+                    /><br />
+                    <label>Enter your last name:</label>
+                    <input
+                        type="text"
+                        // pattern="[0]{1}[5]{1}[0-9]{8}"
+                        // required
+                        placeholder='last name!'
+                        value={lastName}
+                        onChange={(e) => setLastName(e.target.value)}
+                    /><br />
+                    <div>
+                        <input
+                            name="gender"
+                            type="radio"
+                            value="male"
+
+                            ref={val => {
+                                setGender(val);
+                            }}
+                        />
+                        <label>Male</label>
+                        <input
+                            name="gender"
+                            type="radio"
+                            value="Female"
+
+                            ref={val => {
+                                setGender(val);
+                            }}
+                        />
+                        <label>Female</label>
+                    </div>
+                    <label>Enter your Birth Of Date:</label>
+                    <input type='date'
+                        placeholder='Enter BirthDate'
+                        value={rule}
+                        onChange={(e) => setDate(e.target.value)}
+                        name='birthdate'
+                        max={current}
+                    /><br />
+                    <label>Enter your location:</label>
+                    <input
+                        type="text"
+                        // pattern="[0]{1}[5]{1}[0-9]{8}"
+                        // required
+                        placeholder='location!'
+                        value={location}
+                        onChange={(e) => setLocation(e.target.value)}
+                    /><br />
+                    <label>Enter your rule:</label>
+                    <input
+                        type="text"
+                        // pattern="[0]{1}[5]{1}[0-9]{8}"
+                        // required
+                        placeholder='Rule!'
+                        value={rule}
+                        onChange={(e) => setRule(e.target.value)}
+                    /><br />
+                    <label>Insert your Profile's picture:</label>
+                    <input
+                        type="text"
+                        // pattern="[0]{1}[5]{1}[0-9]{8}"
+                        // required
+                        placeholder='Picture!'
+                        value={picture}
+                        onChange={(e) => setPicture(e.target.value)}
+                    /><br />
+
+                    <input type="submit" />
+                </form>
+                <button>Facebook</button><br />
+                <button>Gmail</button><br />
+                <button onClick={handleClick}>already have an account ? sign in here!</button>
             </RegisterFormStyle>
         </div>
     )
@@ -151,8 +245,32 @@ const RegisterFormStyle = styled.div`
   height: 360px;
   width:500px;  
   position:absolute;
-  left:550px;
-  top:40px;
+  left:850px;
+  top:260px;
+  border-radius:30px;
+  ${'' /* display: flex; */}
+  flex-direction:row;
+  ${'' /* justify-content: space-between; */}
+  ${'' /* padding: 0.5rem calc((100vw - 1000px) / 2); */}
+  ${'' /* z-index: 10; */}
+  /* Third Nav */
+  /* justify-content: flex-start; */
+`;
+const FirstRegisterFormStyle = styled.div`
+  ${'' /* background: linear-gradient(135deg, rgb(11,15,67) 5%,rgb(27,100,221) ); */}
+   ${'' /* width: 14px; */}
+  ${'' /* height: 24px; */}
+  ${'' /* background-image: url(data:image/svg+xml;utf8,%3Csvg%20viewBox%3D%220%200%2024%2024%22%20version%3D%221.1%22%20xmlns%3D%22http%3A//www.w3.org/2000/svg%22%20xmlns%3Axlink%3D%22http%3A//www.w3.org/1999/xlink%22%3E%0A%20%20%20%20%3Cdefs%3E%0A%20%20%20%20%20%20%20%20%3Cpath%20d%3D%22M5%2C11%20C5%2C7.691%207.691%2C5%2011%2C5%20C14.309%2C5%2017%2C7.691%2017%2C11%20C17%2C14.309%2014.309%2C17%2011%2C17%20C7.691%2C17%205%2C14.309%205%2C11%20M20.707%2C19.293%20L17.312%2C15.897%20C18.365%2C14.543%2019%2C12.846%2019%2C11%20C19%2C6.589%2015.411%2C3%2011%2C3%20C6.589%2C3%203%2C6.589%203%2C11%20C3%2C15.411%206.589%2C19%2011%2C19%20C12.846%2C19%2014.543%2C18.365%2015.897%2C17.312%20L19.293%2C20.707%20C19.488%2C20.902%2019.744%2C21%2020%2C21%20C20.256%2C21%2020.512%2C20.902%2020.707%2C20.707%20C21.098%2C20.316%2021.098%2C19.684%2020.707%2C19.293%22%20id%3D%22path-1%22%3E%3C/path%3E%0A%20%20%20%20%3C/defs%3E%0A%20%20%20%20%3Cg%20id%3D%22search%22%20stroke%3D%22none%22%20stroke-width%3D%221%22%20fill%3D%22none%22%20fill-rule%3D%22evenodd%22%3E%0A%20%20%20%20%20%20%20%20%3Cmask%20id%3D%22mask-2%22%20fill%3D%22white%22%3E%0A%20%20%20%20%20%20%20%20%20%20%20%20%3Cuse%20xlink%3Ahref%3D%22%23path-1%22%3E%3C/use%3E%0A%20%20%20%20%20%20%20%20%3C/mask%3E%0A%20%20%20%20%20%20%20%20%3Cuse%20id%3D%22%uD83C%uDFA8-Icon-%u0421olor%22%20fill%3D%22%230D1C2E%22%20fill-rule%3D%22nonzero%22%20xlink%3Ahref%3D%22%23path-1%22%3E%3C/use%3E%0A%20%20%20%20%3C/g%3E%0A%3C/svg%3E); */}
+ ${'' /* background-position: 50% 50%; */}
+ ${'' /* background-repeat: no-repeat; */}
+  padding-left: 2rem;
+  background: transparent linear-gradient(150deg,#025fdb 0%,#025fdb 35%,#0b3668 100%) 0% 0% no-repeat padding-box;
+  box-shadow: 0 3px 20px rgb(0 0 0 / 8%);
+  height: 260px;
+  width:360px;  
+  position:absolute;
+  left:50px;
+  top:100px;
   border-radius:30px;
   ${'' /* display: flex; */}
   flex-direction:row;
