663957cfe63b5d9a04d2a3be83faff3092854ec8
OmerShalom <73235082+Omer2041@users.noreply.github.com>
2022-02-23 21:53:46 +0200

    poll controller almost done

--- Server/routes/polls.js
+++ Server/routes/polls.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const {createPoll, getPoll, updatePoll, deletePoll, answerPoll,
-        pollsByID, pollsFollowing} = require("../controllers/poll_controller")
+        pollsFollowing} = require("../controllers/poll_controller")
 
 const router = express.Router();
 
 router.post("/create_poll", createPoll);
-router.get("/get_poll/:poll_id/:user_id", getPoll);
+router.get("/get_poll/:user_id", getPoll);
 router.put("/update_poll/:poll_id/:user_id", updatePoll);
 router.delete("/delete_poll/:poll_id/:user_id", deletePoll);
 router.post("/answer_poll/:poll_id/:user_id/:answer_id", answerPoll);
-router.get("/polls_by_id/:user_id", pollsByID);
 router.get("/poll_feed/:user_id", pollsFollowing);
 
 module.exports = {
