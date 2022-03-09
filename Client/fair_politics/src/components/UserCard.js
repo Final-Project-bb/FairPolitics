684428d6c0b69e2ce8b963b6dd7619186c70c3a0
talschreiber95 <talfreestyle@gmail.com>
2022-03-09 11:49:03 +0200

    client: login controller works successfully

--- Client/fair_politics/src/components/UserCard.js
+++ Client/fair_politics/src/components/UserCard.js
@@ -1,49 +1,108 @@
 import React, { useContext, useState, useEffect } from "react";
 import Header from "./Header";
 import Loading from "./Loading";
+import { useHistory } from "react-router-dom";
+
 import styled from "styled-components";
 import { AppContext } from "./Context";
 
-const UserCard = ({ user_info }) => {
-  const { user_details, loading, setLoading, followings, followers } =
+const UserCard = ({ user_info, inFollowing, inSearch }) => {
+  const { setFollowerDetails, setFollowingDetails, setFollowers, setFollowings, user_details, loading, setLoading, followings, followers } =
     useContext(AppContext);
+  const history = useHistory();
 
   const [picturePress, setPicturePress] = useState(false);
   const [isFollow, setIsFollow] = useState(false);
+  const [flag, setFlag] = useState(true);
 
+  // const flag= true;
   useEffect(() => {
+    fetchFollow();
+    let following_ids = followings.map((user) => user.user_following_id);
+    fetchUserDetailsById(following_ids, true)
     const checkFollow = () => {
       let followState = false;
       followState = followings
         .map((user) => user.user_following_id)
-        .map((id) => {
-          if (id === user_info.user_id) {
-            return true;
-          }
-        });
-        console.log(followState)
+        .every((id) => id == user_info.user_id);
       setIsFollow(followState);
+      if (followings.length === 0) {
+        setIsFollow(false);
+        if (inFollowing) {
+          setFlag(false);
+        }
+      }
+      else {
+        if (inFollowing) {
+          if (followState) {
+            setFlag(true);
+          }
+          else {
+            setFlag(false);
+          }
+        }
+      }
     };
     checkFollow();
-  }, []);
+
+  }, [flag, isFollow]);
   // here should add and remove follow from db.
   const followUser = (e) => {
-    e.preventDefault();
+    // e.preventDefault();
     setLoading(true);
+
     if (isFollow) {
-      // add follow to db and update the usestate
-      addFollowDb();
-      setIsFollow(!isFollow);
-    } else {
       // remove follow from db and update the usestate
       removeFollowDb();
-      setIsFollow(!isFollow);
+    } else {
+      // add follow to db and update the usestate
+      addFollowDb();
+      // setIsFollow(!isFollow);
     }
+    if (inFollowing) {
+      history.push("/profile/follower");
+      history.push("/profile/following");
+    }
+    else {
+      if (inSearch) {
+        history.push("/profile/following");
+        history.push("/search");
+      } else {
+        history.push("/profile/following");
+        history.push("/profile/follower");
+      }
+    }
+    setIsFollow(!isFollow);
     setLoading(false);
 
     //here should be following
   };
+  const fetchFollow = async () => {
+    const response = await fetch(`http://localhost:4000/api/get_follow/${user_details.user_id}`);
+    const data = await response.json();
+    // console.log(data);
+    await setFollowings(data.following);
+    await setFollowers(data.follower);
+  }
 
+  const fetchUserDetailsById = async (ids, isFollowing) => {
+    if (ids.length === 0) {
+      return;
+    }
+    const response = await fetch("http://localhost:4000/api/get_users_by_ids", {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(ids),
+    });
+    const data = await response.json();
+    if (isFollowing) {
+      await setFollowingDetails(data.result);
+      // console.log(data.result);
+    } else {
+      await setFollowerDetails(data.result);
+      // console.log(data.result);
+    }
+  };
   const addFollowDb = async () => {
     const ids = {
       user_id: user_details.user_id,
@@ -72,45 +131,51 @@ const UserCard = ({ user_info }) => {
   };
   return (
     <div>
-      <UserCardStyle>
-        <div style={styles.all}>
-          <div style={styles.semiDetails}>
-            <div style={styles.name}>
-              {user_info.first_name} {user_info.last_name}
+      {flag &&
+        <UserCardStyle>
+          <div style={styles.all}>
+            <div style={styles.semiDetails}>
+              <div style={styles.name}>
+                {user_info.first_name} {user_info.last_name}
+              </div>
+              <div>
+                {user_info.gender} , {user_info.age}
+              </div>
+              <div>
+                Working it: {user_info.job_title} living in {user_info.city}
+              </div>
+              <div>{user_info.semi_description}</div>
+              <button onClick={() => console.log(`is follow: ${isFollow} ${user_info.user_id}`)}>here</button>
             </div>
-            <div>
-              {user_info.gender} , {user_info.age}
+            <div style={styles.profileHead}>
+              <img
+                src={require("../images/profilePicExmple.jpg")}
+                alt='logo'
+                style={{
+                  borderRadius: !picturePress ? 350 : 0,
+                  position: !picturePress ? "relative" : "absolute",
+                  left: 20,
+                  height: !picturePress ? 50 : 200,
+                  width: !picturePress ? 50 : 200,
+                }}
+                onClickCapture={() => {
+                  setPicturePress(!picturePress);
+                }}
+              />
             </div>
-            <div>
-              Working it: {user_info.job_title} living in {user_info.city}
-            </div>
-            <div>{user_info.semi_description}</div>
           </div>
-          <div style={styles.profileHead}>
-            <img
-              src={require("../images/profilePicExmple.jpg")}
-              alt='logo'
-              style={{
-                borderRadius: !picturePress ? 350 : 0,
-                position: !picturePress ? "relative" : "absolute",
-                left: 20,
-                height: !picturePress ? 50 : 200,
-                width: !picturePress ? 50 : 200,
-              }}
-              onClickCapture={() => {
-                setPicturePress(!picturePress);
-              }}
-            />
-          </div>
-        </div>
-        <button style={styles.follow} onClick={(e) => followUser(e)}>
-          {!isFollow ? "follow" : "unfollow"}!
-        </button>
-      </UserCardStyle>
+          <button style={styles.follow} onClick={(e) => followUser(e)}>
+            {isFollow ? "unfollow" : "follow"}!
+          </button>
+        </UserCardStyle>
+      }
     </div>
   );
 };
-
+UserCard.defaultProps = {
+  inFollowing: false,
+  inSearch: false,
+}
 const styles = {
   name: {
     display: "flex",
