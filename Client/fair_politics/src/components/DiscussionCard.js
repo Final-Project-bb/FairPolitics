55173859c3795c2c69db995661e1a7de237a79b9
talschreiber95 <talfreestyle@gmail.com>
2022-03-13 22:30:27 +0200

    client icons added

--- Client/fair_politics/src/components/DiscussionCard.js
+++ Client/fair_politics/src/components/DiscussionCard.js
@@ -1,8 +1,8 @@
 import React, { useState, useContext } from "react";
 import styled from "styled-components";
 import { AppContext } from "./Context";
-
-const DiscussionCard = ({ DiscussionCards }) => {
+import { FaRegEdit, FaTrashAlt } from "react-icons/fa";
+const DiscussionCard = ({ DiscussionCards, inProfile }) => {
   const [commentsButtonId, setCommentsButtonId] = useState(0);
   const [commentsButton, setCommentsButton] = useState(false);
   const [height, setHeight] = useState(0);
@@ -27,6 +27,12 @@ const DiscussionCard = ({ DiscussionCards }) => {
       handleDis.Likes.push(userId);
     }
   };
+  const editDiscussion = (e) => {
+    alert("edit Click");
+  }
+  const removeDiscussion = (e) => {
+    alert("remove Click");
+  }
   return (
     <div style={styles.head}>
       <div style={styles.title}>Discussion Card Side</div>
@@ -38,6 +44,11 @@ const DiscussionCard = ({ DiscussionCards }) => {
             style={{
               height: (item.description.length / 3) * 32 + height + 35,
             }}>
+            {inProfile && <div>
+              <FaRegEdit onClick={(e) => editDiscussion(e)} />
+              <FaTrashAlt onClick={(e) => removeDiscussion(e)} />
+            </div>
+            }
             <div style={styles.title}>
               {item.post_id} {item.title}{" "}
             </div>
@@ -80,8 +91,8 @@ const DiscussionCard = ({ DiscussionCards }) => {
                 }}
                 type='text'
                 placeholder='add comment'
-                // value={search}
-                // onChange={(e) => setSearch(e.target.value)}
+              // value={search}
+              // onChange={(e) => setSearch(e.target.value)}
               />
 
               {/* <input type="submit" style={{ color: "white", backgroundColor: "black" }} /> */}
@@ -92,6 +103,10 @@ const DiscussionCard = ({ DiscussionCards }) => {
     </div>
   );
 };
+
+DiscussionCard.deafult = {
+  inProfile: false
+}
 
 const styles = {
   head: {
@@ -139,13 +154,11 @@ const styles = {
 };
 
 const DisCard = styled.div`
-  ${
-    "" /* background: linear-gradient(135deg, rgb(11,15,67) 5%,rgb(27,100,221) ); */
+  ${"" /* background: linear-gradient(135deg, rgb(11,15,67) 5%,rgb(27,100,221) ); */
   }
   ${"" /* width: 14px; */}
   ${"" /* height: 24px; */}
-  ${
-    "" /* background-image: url(data:image/svg+xml;utf8,%3Csvg%20viewBox%3D%220%200%2024%2024%22%20version%3D%221.1%22%20xmlns%3D%22http%3A//www.w3.org/2000/svg%22%20xmlns%3Axlink%3D%22http%3A//www.w3.org/1999/xlink%22%3E%0A%20%20%20%20%3Cdefs%3E%0A%20%20%20%20%20%20%20%20%3Cpath%20d%3D%22M5%2C11%20C5%2C7.691%207.691%2C5%2011%2C5%20C14.309%2C5%2017%2C7.691%2017%2C11%20C17%2C14.309%2014.309%2C17%2011%2C17%20C7.691%2C17%205%2C14.309%205%2C11%20M20.707%2C19.293%20L17.312%2C15.897%20C18.365%2C14.543%2019%2C12.846%2019%2C11%20C19%2C6.589%2015.411%2C3%2011%2C3%20C6.589%2C3%203%2C6.589%203%2C11%20C3%2C15.411%206.589%2C19%2011%2C19%20C12.846%2C19%2014.543%2C18.365%2015.897%2C17.312%20L19.293%2C20.707%20C19.488%2C20.902%2019.744%2C21%2020%2C21%20C20.256%2C21%2020.512%2C20.902%2020.707%2C20.707%20C21.098%2C20.316%2021.098%2C19.684%2020.707%2C19.293%22%20id%3D%22path-1%22%3E%3C/path%3E%0A%20%20%20%20%3C/defs%3E%0A%20%20%20%20%3Cg%20id%3D%22search%22%20stroke%3D%22none%22%20stroke-width%3D%221%22%20fill%3D%22none%22%20fill-rule%3D%22evenodd%22%3E%0A%20%20%20%20%20%20%20%20%3Cmask%20id%3D%22mask-2%22%20fill%3D%22white%22%3E%0A%20%20%20%20%20%20%20%20%20%20%20%20%3Cuse%20xlink%3Ahref%3D%22%23path-1%22%3E%3C/use%3E%0A%20%20%20%20%20%20%20%20%3C/mask%3E%0A%20%20%20%20%20%20%20%20%3Cuse%20id%3D%22%uD83C%uDFA8-Icon-%u0421olor%22%20fill%3D%22%230D1C2E%22%20fill-rule%3D%22nonzero%22%20xlink%3Ahref%3D%22%23path-1%22%3E%3C/use%3E%0A%20%20%20%20%3C/g%3E%0A%3C/svg%3E); */
+  ${"" /* background-image: url(data:image/svg+xml;utf8,%3Csvg%20viewBox%3D%220%200%2024%2024%22%20version%3D%221.1%22%20xmlns%3D%22http%3A//www.w3.org/2000/svg%22%20xmlns%3Axlink%3D%22http%3A//www.w3.org/1999/xlink%22%3E%0A%20%20%20%20%3Cdefs%3E%0A%20%20%20%20%20%20%20%20%3Cpath%20d%3D%22M5%2C11%20C5%2C7.691%207.691%2C5%2011%2C5%20C14.309%2C5%2017%2C7.691%2017%2C11%20C17%2C14.309%2014.309%2C17%2011%2C17%20C7.691%2C17%205%2C14.309%205%2C11%20M20.707%2C19.293%20L17.312%2C15.897%20C18.365%2C14.543%2019%2C12.846%2019%2C11%20C19%2C6.589%2015.411%2C3%2011%2C3%20C6.589%2C3%203%2C6.589%203%2C11%20C3%2C15.411%206.589%2C19%2011%2C19%20C12.846%2C19%2014.543%2C18.365%2015.897%2C17.312%20L19.293%2C20.707%20C19.488%2C20.902%2019.744%2C21%2020%2C21%20C20.256%2C21%2020.512%2C20.902%2020.707%2C20.707%20C21.098%2C20.316%2021.098%2C19.684%2020.707%2C19.293%22%20id%3D%22path-1%22%3E%3C/path%3E%0A%20%20%20%20%3C/defs%3E%0A%20%20%20%20%3Cg%20id%3D%22search%22%20stroke%3D%22none%22%20stroke-width%3D%221%22%20fill%3D%22none%22%20fill-rule%3D%22evenodd%22%3E%0A%20%20%20%20%20%20%20%20%3Cmask%20id%3D%22mask-2%22%20fill%3D%22white%22%3E%0A%20%20%20%20%20%20%20%20%20%20%20%20%3Cuse%20xlink%3Ahref%3D%22%23path-1%22%3E%3C/use%3E%0A%20%20%20%20%20%20%20%20%3C/mask%3E%0A%20%20%20%20%20%20%20%20%3Cuse%20id%3D%22%uD83C%uDFA8-Icon-%u0421olor%22%20fill%3D%22%230D1C2E%22%20fill-rule%3D%22nonzero%22%20xlink%3Ahref%3D%22%23path-1%22%3E%3C/use%3E%0A%20%20%20%20%3C/g%3E%0A%3C/svg%3E); */
   }
  ${"" /* background-position: 50% 50%; */}
  ${"" /* background-repeat: no-repeat; */}
--- Client/fair_politics/src/components/FeedbackCard.js
+++ Client/fair_politics/src/components/FeedbackCard.js
@@ -1,8 +1,8 @@
 import React, { useState, useContext } from "react";
 import styled from "styled-components";
 import { AppContext } from "./Context";
-
-const FeedbackCard = ({FeedbackCards}) => {
+import { FaRegEdit, FaTrashAlt } from "react-icons/fa";
+const FeedbackCard = ({ FeedbackCards, inProfile }) => {
   const [commentsButtonId, setCommentsButtonId] = useState(0);
   const [commentsButton, setCommentsButton] = useState(false);
   const [height, setHeight] = useState(0);
@@ -29,45 +29,58 @@ const FeedbackCard = ({FeedbackCards}) => {
     // setCommentsButtonId()
     // setCommentsButton(!commentsButton);
   };
-  const addAnswer = () => {};
-
+  const addAnswer = () => { };
+  const editPoll = (e) => {
+    alert("edit Click");
+  }
+  const removePoll = (e) => {
+    alert("remove Click");
+  }
   return (
     <div style={styles.head}>
       <div style={styles.title}>Feedback Card Side</div>
       {FeedbackCards.map((item) => {
-          return (
-            <PollCard
-              key={item.poll_id}
-              style={{ height: (item.description.length / 3) * 40 + height }}>
-              <div style={styles.title}>
-                {item.poll_id} {item.title}{" "}
-              </div>
-              <div style={styles.description}>{item.description}</div>
-              <div style={styles.cardFooter}>
-                <form>
-                  <div style={styles.question}>
-                    {item.answers.map((answer) => (
-                  <div key={answer.answer_id}>
-                    {answer.title}
+        return (
+          <PollCard
+            key={item.poll_id}
+            style={{ height: (item.description.length / 2.5) * 40 + height }}>
+            {inProfile && <div>
+              <FaRegEdit onClick={(e) => editPoll(e)} />
+              <FaTrashAlt onClick={(e) => removePoll(e)} />
+            </div>
+            }
+            <div style={styles.title}>
+              {item.poll_id} {item.title}{" "}
+            </div>
+
+            <div style={styles.description}>{item.description}</div>
+            <div style={styles.cardFooter}>
+              <form>
+                <div style={styles.question}>
+                  {item.answers.map((answer) => (
+                    <div key={answer.answer_id}>
+                      {answer.title}
                       <div>
                         <label>
                           <input type='checkbox' />
                           {answer.answer}
                         </label>
                       </div>
-                  </div>
-                ))}
-                  </div>
-                  <button onClick={() => addAnswer(item)}>submit!</button>
-                </form>
-              </div>
-            </PollCard>
-          );
+                    </div>
+                  ))}
+                </div>
+                <button onClick={() => addAnswer(item)}>submit!</button>
+              </form>
+            </div>
+          </PollCard>
+        );
       })}
     </div>
   );
 };
-
+FeedbackCard.deafult = {
+  inProfile: false
+}
 const styles = {
   head: {
     display: "flex",
@@ -120,13 +133,11 @@ const styles = {
 };
 
 const PollCard = styled.div`
-  ${
-    "" /* background: linear-gradient(135deg, rgb(11,15,67) 5%,rgb(27,100,221) ); */
+  ${"" /* background: linear-gradient(135deg, rgb(11,15,67) 5%,rgb(27,100,221) ); */
   }
   ${"" /* width: 14px; */}
   ${"" /* height: 24px; */}
-  ${
-    "" /* background-image: url(data:image/svg+xml;utf8,%3Csvg%20viewBox%3D%220%200%2024%2024%22%20version%3D%221.1%22%20xmlns%3D%22http%3A//www.w3.org/2000/svg%22%20xmlns%3Axlink%3D%22http%3A//www.w3.org/1999/xlink%22%3E%0A%20%20%20%20%3Cdefs%3E%0A%20%20%20%20%20%20%20%20%3Cpath%20d%3D%22M5%2C11%20C5%2C7.691%207.691%2C5%2011%2C5%20C14.309%2C5%2017%2C7.691%2017%2C11%20C17%2C14.309%2014.309%2C17%2011%2C17%20C7.691%2C17%205%2C14.309%205%2C11%20M20.707%2C19.293%20L17.312%2C15.897%20C18.365%2C14.543%2019%2C12.846%2019%2C11%20C19%2C6.589%2015.411%2C3%2011%2C3%20C6.589%2C3%203%2C6.589%203%2C11%20C3%2C15.411%206.589%2C19%2011%2C19%20C12.846%2C19%2014.543%2C18.365%2015.897%2C17.312%20L19.293%2C20.707%20C19.488%2C20.902%2019.744%2C21%2020%2C21%20C20.256%2C21%2020.512%2C20.902%2020.707%2C20.707%20C21.098%2C20.316%2021.098%2C19.684%2020.707%2C19.293%22%20id%3D%22path-1%22%3E%3C/path%3E%0A%20%20%20%20%3C/defs%3E%0A%20%20%20%20%3Cg%20id%3D%22search%22%20stroke%3D%22none%22%20stroke-width%3D%221%22%20fill%3D%22none%22%20fill-rule%3D%22evenodd%22%3E%0A%20%20%20%20%20%20%20%20%3Cmask%20id%3D%22mask-2%22%20fill%3D%22white%22%3E%0A%20%20%20%20%20%20%20%20%20%20%20%20%3Cuse%20xlink%3Ahref%3D%22%23path-1%22%3E%3C/use%3E%0A%20%20%20%20%20%20%20%20%3C/mask%3E%0A%20%20%20%20%20%20%20%20%3Cuse%20id%3D%22%uD83C%uDFA8-Icon-%u0421olor%22%20fill%3D%22%230D1C2E%22%20fill-rule%3D%22nonzero%22%20xlink%3Ahref%3D%22%23path-1%22%3E%3C/use%3E%0A%20%20%20%20%3C/g%3E%0A%3C/svg%3E); */
+  ${"" /* background-image: url(data:image/svg+xml;utf8,%3Csvg%20viewBox%3D%220%200%2024%2024%22%20version%3D%221.1%22%20xmlns%3D%22http%3A//www.w3.org/2000/svg%22%20xmlns%3Axlink%3D%22http%3A//www.w3.org/1999/xlink%22%3E%0A%20%20%20%20%3Cdefs%3E%0A%20%20%20%20%20%20%20%20%3Cpath%20d%3D%22M5%2C11%20C5%2C7.691%207.691%2C5%2011%2C5%20C14.309%2C5%2017%2C7.691%2017%2C11%20C17%2C14.309%2014.309%2C17%2011%2C17%20C7.691%2C17%205%2C14.309%205%2C11%20M20.707%2C19.293%20L17.312%2C15.897%20C18.365%2C14.543%2019%2C12.846%2019%2C11%20C19%2C6.589%2015.411%2C3%2011%2C3%20C6.589%2C3%203%2C6.589%203%2C11%20C3%2C15.411%206.589%2C19%2011%2C19%20C12.846%2C19%2014.543%2C18.365%2015.897%2C17.312%20L19.293%2C20.707%20C19.488%2C20.902%2019.744%2C21%2020%2C21%20C20.256%2C21%2020.512%2C20.902%2020.707%2C20.707%20C21.098%2C20.316%2021.098%2C19.684%2020.707%2C19.293%22%20id%3D%22path-1%22%3E%3C/path%3E%0A%20%20%20%20%3C/defs%3E%0A%20%20%20%20%3Cg%20id%3D%22search%22%20stroke%3D%22none%22%20stroke-width%3D%221%22%20fill%3D%22none%22%20fill-rule%3D%22evenodd%22%3E%0A%20%20%20%20%20%20%20%20%3Cmask%20id%3D%22mask-2%22%20fill%3D%22white%22%3E%0A%20%20%20%20%20%20%20%20%20%20%20%20%3Cuse%20xlink%3Ahref%3D%22%23path-1%22%3E%3C/use%3E%0A%20%20%20%20%20%20%20%20%3C/mask%3E%0A%20%20%20%20%20%20%20%20%3Cuse%20id%3D%22%uD83C%uDFA8-Icon-%u0421olor%22%20fill%3D%22%230D1C2E%22%20fill-rule%3D%22nonzero%22%20xlink%3Ahref%3D%22%23path-1%22%3E%3C/use%3E%0A%20%20%20%20%3C/g%3E%0A%3C/svg%3E); */
   }
  ${"" /* background-position: 50% 50%; */}
  ${"" /* background-repeat: no-repeat; */}
