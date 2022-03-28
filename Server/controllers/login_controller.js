ab0e8873360803bb1ac48909dd52ae0663cc1627
shaiBonfil <shaibonfil500@gmail.com>
2022-03-28 20:50:41 +0300

    passport not finished

--- Server/controllers/login_controller.js
+++ Server/controllers/login_controller.js
@@ -136,7 +136,7 @@ const auth = (req, res) => {
 
   var mailOptions = {
     from: "fairpolitics5@gmail.com",
-    to: "shaibonfil500@gmail.com",
+    to: req.body.email,
     subject: "fair politics sign up",
     text: `your temporary password is ${code}`,
   };
--- Server/routes/login.js
+++ Server/routes/login.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+// const passport = require("passport");
 const connection = require("../lib/db");
 
 const {
@@ -39,6 +40,51 @@ router.delete("/remove_following", removeFollowing); //works fine
 
 router.post("/choose_algorithm/:user_id/:algorithm_id", chooseAlgorithm); //works fine
 router.get("/get_algorithm/:user_id", getChosenAlgorithm); //works fine
+
+
+// const CLIENT_URL = "http://localhost:4000/";
+// router.get("/login/success", (req, res) => {
+//   if (req.user) {
+//     res.status(200).json({
+//       success: true,
+//       message: "successfull",
+//       user: req.user,
+//       //   cookies: req.cookies
+//     });
+//   }
+// });
+
+// router.get("/login/failed", (req, res) => {
+//   res.status(401).json({
+//     success: false,
+//     message: "failure",
+//   });
+// });
+
+// router.get("/logout", (req, res) => {
+//   req.logout();
+//   res.redirect(CLIENT_URL);
+// });
+
+// router.get("/google", passport.authenticate("google", { scope: ["profile"] }));
+
+// router.get(
+//   "/google/callback",
+//   passport.authenticate("google", {
+//     successRedirect: CLIENT_URL,
+//     failureRedirect: "/login/failed",
+//   })
+// );
+
+// router.get("/facebook", passport.authenticate("facebook", { scope: ["profile"] }));
+
+// router.get(
+//   "/facebook/callback",
+//   passport.authenticate("facebook", {
+//     successRedirect: CLIENT_URL,
+//     failureRedirect: "/login/failed",
+//   })
+// );
 
 module.exports = {
   routes: router,
